a64b05e906d5ec333a4fe40529e2c0f98e30e3d5
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-05-08 00:56:04 +0600

    Add files via upload

---
+++ Using C++/CodeForces/ICPC Assiut University Community/Sheet 1 (Data Type - Conditions)/D - Difference.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	long long int a,b,c,d,diff;
+	cin>>a>>b>>c>>d;
+	
+	diff = (a*b) - (c*d);
+	
+	cout<<"Difference = "<<diff;
+	
+	return 0;
+}
