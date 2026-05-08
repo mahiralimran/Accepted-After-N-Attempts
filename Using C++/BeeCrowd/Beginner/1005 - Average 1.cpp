98ca3a069e778c2ee30f959b854c3be03ff228dc
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-05-08 20:15:14 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1005 - Average 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	double a,b,avg;
+	cin>>a>>b;
+	
+	avg = (a * 3.5 + b * 7.5) / 11;
+	
+	cout<<"MEDIA = "<<fixed<<setprecision(5)<<avg<<endl;
+	
+	return 0;
+}
