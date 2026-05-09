bc1965aae9f8b62986510f5ac172dc9d973c9de2
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-05-09 13:49:47 +0600

    Add files via upload

---
+++ Using C++/CodeForces/ICPC Assiut University Community/Sheet 1 (Data Type - Conditions)/L - The Brothers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string b1,b2,sn1,sn2;
+	cin>>b1>>sn1>>b2>>sn2;
+	
+	if(sn1==sn2){
+		cout<<"ARE Brothers"<<endl;
+	}
+	else{
+		cout<<"NOT"<<endl;
+	}
+	
+	return 0;
+}
