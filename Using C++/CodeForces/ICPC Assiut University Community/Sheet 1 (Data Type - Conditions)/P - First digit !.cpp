535538ce26d8341456e5050a371cec0ea7ffccc0
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-05-10 22:39:35 +0600

    Add files via upload

---
+++ Using C++/CodeForces/ICPC Assiut University Community/Sheet 1 (Data Type - Conditions)/P - First digit !.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	
+	int fd = n/1000;
+	
+	if(fd%2==0){
+		cout<<"EVEN"<<endl;
+	}
+	else{
+		cout<<"ODD"<<endl;
+	}
+	
+	return 0;
+}
