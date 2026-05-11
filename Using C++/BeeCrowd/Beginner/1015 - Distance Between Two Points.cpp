da331f1b68a3330442f2071969cfd10aa766c381
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-05-11 12:27:52 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1015 - Distance Between Two Points.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	double x1,x2,y1,y2;
+	cin>>x1>>y1;
+	cin>>x2>>y2;
+	
+	double dis = sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+	
+	cout<<fixed<<setprecision(4)<<dis<<endl;
+	
+	return 0;
+}
