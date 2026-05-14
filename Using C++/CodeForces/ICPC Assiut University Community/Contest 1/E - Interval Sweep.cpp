5617a9576a49490e04fb0640ccd129b8431a4229
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-05-14 22:38:39 +0600

    Add files via upload

---
+++ Using C++/CodeForces/ICPC Assiut University Community/Contest 1/E - Interval Sweep.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	
+	if(abs(a-b)<=1){
+		cout<<"YES"<<endl;
+	}
+	else{
+		cout<<"NO"<<endl;
+	}
+	
+	return 0;
+}
