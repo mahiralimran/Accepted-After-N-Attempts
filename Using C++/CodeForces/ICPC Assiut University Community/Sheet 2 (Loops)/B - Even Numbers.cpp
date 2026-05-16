609c859e220e9379e9edef98412c14e6b9d6e0ac
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-05-16 13:52:18 +0600

    Add files via upload

---
+++ Using C++/CodeForces/ICPC Assiut University Community/Sheet 2 (Loops)/B - Even Numbers.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,cnt=0;
+	cin>>n;
+	
+	for(int i=1; i<=n; i++){
+		if(i%2==0){
+			cnt++;
+			cout<<i<<endl;
+		}
+	}
+	
+	if(cnt==0){
+		cout<< -1 <<endl;
+	}
+	
+	return 0;
+}
