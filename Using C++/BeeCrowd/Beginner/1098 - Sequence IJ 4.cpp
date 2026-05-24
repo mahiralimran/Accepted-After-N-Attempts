7174af3eaf81e9539e1f0137c7b44b954410b9de
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-05-24 20:14:08 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1098 - Sequence IJ 4.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double i,j;
+
+    for(i=0; i<=2.0; i+=0.2)
+    {
+        for(j=1; j<=3; j++)
+        {
+            cout << "I=" << i << " J=" << j+i << endl;
+        }
+    }
+
+    return 0;
+}
