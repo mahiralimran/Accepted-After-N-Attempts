1ea284c7fe21bae332a86b437eada85976b12e31
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-06-09 17:48:38 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1172 - Array Replacement I.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int arr[10];
+	
+	for(int i=0; i<10; i++){
+		cin >> arr[i];
+		
+		if(arr[i]<=0){
+			arr[i] = 1;
+		}
+		
+		cout << "X[" << i << "] = " << arr[i] << endl;
+	}
+	
+	return 0;
+}
