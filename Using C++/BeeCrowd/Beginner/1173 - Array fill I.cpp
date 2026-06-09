c78c51579fa1147867efdddc6946b1f64a604874
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-06-09 17:53:13 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1173 - Array fill I.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int arr[10];
+	int n;
+	cin >> n;
+	
+	arr[0] = n;
+	
+	for(int i=1; i<10; i++){
+		arr[i] = 2 * arr[i-1];
+	}
+	
+	for(int i=0; i<10; i++){
+		cout << "N[" << i << "] = " << arr[i] << endl;
+	}
+	
+	return 0;
+}
