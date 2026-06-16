6234b5c69c3d153c7d95e331ed4f2a736ff55218
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-06-16 20:25:37 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1187 - Top Area.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	double arr[12][12];
+	string task;
+	cin >> task;
+	
+	for(int i=0; i<12; i++){
+		for(int j=0; j<12; j++){
+			cin >> arr[i][j];
+		}  
+	}
+	
+	double sum = 0;
+	for(int i=0; i<12; i++){
+    	for(int j=0; j<12; j++){
+        	if(i < 5 && j > i && j < 11-i){
+            	sum += arr[i][j];
+        	}
+		}
+	}
+
+	if(task == "S")
+    	cout << fixed << setprecision(1) << sum << endl;
+	else
+    	cout << fixed << setprecision(1) << sum/30.0 << endl;
+	
+	return 0;
+}
