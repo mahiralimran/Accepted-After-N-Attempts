7337ff0765bca8682821e98bfe467b2dbfe91c17
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-06-19 02:44:48 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1759 - Ho Ho Ho.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	
+	for(int i=1; i<=n; i++){
+		cout << "Ho";
+		if(i==n){
+			cout << "!" << endl;
+		}
+		else{
+			cout << " ";
+		}
+	}
+	
+	return 0;
+}
