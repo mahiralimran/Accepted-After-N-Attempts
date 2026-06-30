1c61c7576e36b83676b15399c96dfd1d9f17ba2e
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-06-30 20:52:44 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1865 - Mjölnir.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin >> t;
+	
+	while(t--){
+		string str;
+		int n;
+		cin >> str >> n;
+		
+		if(str == "Thor"){
+			cout << "Y" << endl;
+		}
+		else{
+			cout << "N" << endl;
+		}
+	}
+	
+	return 0;
+}
