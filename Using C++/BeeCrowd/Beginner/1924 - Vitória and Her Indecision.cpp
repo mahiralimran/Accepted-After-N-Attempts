3d2bbbc454bb25ebbbeba0ee00a929179f1d7909
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-07-01 12:07:12 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1924 - Vitória and Her Indecision.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin >> t;
+	
+	while(t--){
+		string str;
+		getline(cin,str);
+	}
+	
+	cout << "Ciencia da Computacao" << endl;
+	
+	return 0;
+}
