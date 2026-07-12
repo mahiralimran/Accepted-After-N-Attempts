e3e4611e5843daa9bb48ee6fc63ac77ef0108af8
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-07-12 21:51:10 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1958 - Scientific Notation.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double x;
+    cin >> x;
+
+    cout << uppercase << scientific << showpos << setprecision(4) << x << endl;
+
+    return 0;
+}
