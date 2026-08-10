c08f4c88fe62d82e1dc18e9e0c0fac5367ea9228
Mahir Al Imran <sagor15-5304@diu.edu.bd>
2026-08-10 11:20:16 +0600

    Add files via upload

---
+++ Using C++/BeeCrowd/Beginner/1960 - Roman Numerals for Page Numbers.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    while(n != 0)
+    {
+        if(n >= 1000)
+        {
+            cout << "M";
+            n -= 1000;
+        }
+        else if(n >= 900)
+        {
+            cout << "CM";
+            n -= 900;
+        }
+        else if(n >= 500)
+        {
+            cout << "D";
+            n -= 500;
+        }
+        else if(n >= 400)
+        {
+            cout << "CD";
+            n -= 400;
+        }
+        else if(n >= 100)
+        {
+            cout << "C";
+            n -= 100;
+        }
+        else if(n >= 90)
+        {
+            cout << "XC";
+            n -= 90;
+        }
+        else if(n >= 50)
+        {
+            cout << "L";
+            n -= 50;
+        }
+        else if(n >= 40)
+        {
+            cout << "XL";
+            n -= 40;
+        }
+        else if(n >= 10)
+        {
+            cout << "X";
+            n -= 10;
+        }
+        else if(n >= 9)
+        {
+            cout << "IX";
+            n -= 9;
+        }
+        else if(n >= 5)
+        {
+            cout << "V";
+            n -= 5;
+        }
+        else if(n >= 4)
+        {
+            cout << "IV";
+            n -= 4;
+        }
+        else if(n >= 1)
+        {
+            cout << "I";
+            n -= 1;
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
